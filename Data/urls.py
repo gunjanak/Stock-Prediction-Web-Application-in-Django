72904ea97bf8ddd8778a9e1c1782b16e7327088c
gunjanak <janak.klal@gmail.com>
2023-12-04 20:34:38 +0545

    Dropdown menu for stock symbols done

--- Data/urls.py
+++ Data/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import nepse_symbols_view,stock_dataFrame_view
+from .views import nepse_symbols_view,stock_dataFrame_view,charts_view
 
 urlpatterns = [
     path('symbols/',nepse_symbols_view,name='nepse_symbols'),
     path('stock/',stock_dataFrame_view,name="stock_dataframe"),
+    path('chart/',charts_view,name='chart_view'),
 ]
--- Data/views.py
+++ Data/views.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from django.shortcuts import render
 from django.http import JsonResponse
 
@@ -14,10 +16,19 @@ def nepse_symbols_view(request):
 
 def stock_dataFrame_view(request):
     df = stock_dataFrame("NTC")
+    print(df.head())
+    df = df.reset_index()
+    df['timestamp'] = pd.to_datetime(df['Date'])
+    df['Date'] = df['timestamp'].dt.date
 
     #convert dataframe to a list of dictionaries
     df_dict_list = df.to_dict('records')
+    print(df_dict_list)
 
     context = {"data":df_dict_list}
 
     return render(request,"stock_data.html",context)
+
+
+def charts_view(request):
+    return render(request,"charts.html")
